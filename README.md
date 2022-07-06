12668981e247d4847d38c30440af7de4311fa55b
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-07-06 17:08:16 +0900

    buildアクションを追加

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ $ poetry install
 $ poetry run start
 ```
 
+# リリース
+
+```sh
+$ poetry build
+$ pip install /src/app/OS-lecture-Support-tool/dist/OS_lecture_Support_tool-0.1.0-py3-none-any.whl
+$ os_lecture_support_tool
+```
+
 ## 参考
 
 https://cocoatomo.github.io/poetry-ja/pyproject/
--- src/OS-lecture-Support-tool/os_lecture_support_tool/main.py
+++ src/OS-lecture-Support-tool/os_lecture_support_tool/main.py
@@ -1,5 +1,15 @@
+import os
+import yaml
+import sys
+
+def open_yaml(file_name: str) -> object:
+  with open(f"{os.getcwd()}/../{file_name}") as file:
+    obj = yaml.safe_load(file)
+    return obj
+
 def main():
-  print('Hello, World!')
+  obj = open_yaml("test.yml")
+  print(obj["name"])
 
 if __name__ == '__main__':
   main()
