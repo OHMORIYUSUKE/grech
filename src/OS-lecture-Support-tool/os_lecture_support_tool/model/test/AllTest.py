b97e0f451639da1b16a254618efe94fa0fce3b42
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-07-28 23:40:44 +0900

    fix

--- src/OS-lecture-Support-tool/os_lecture_support_tool/model/test/AllTest.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-from os_lecture_support_tool.model.test.Chapter import Chapter
-
-# チャプターに存在する複数のテスト
-class Alltest(BaseModel):
-    all_test: List[Chapter]
--- src/OS-lecture-Support-tool/os_lecture_support_tool/model/test/Chapter.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-from os_lecture_support_tool.model.test.Test import Test
-
-# チャプターに存在する複数のテスト
-class Chapter(BaseModel):
-    name: str
-    tests: List[Test]
